d0d14113b7a9856842a1ada6ff09510f1bd1d584
roseak <rose.kohn@gmail.com>
2016-01-27 10:09:06 -0700

    Add fun store id name and the route to go to the app on submit by using the history mixin.

--- catch-of-the-day/tutorial/scripts/main.js
+++ catch-of-the-day/tutorial/scripts/main.js
@@ -5,7 +5,10 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+var h = require('./helpers');
 
 /*
   App
@@ -74,11 +77,17 @@ var Inventory = React.createClass({
 */
 
 var StorePicker = React.createClass({
+  mixins: [History],
+  goToStore: function(event) {
+    event.preventDefault();
+    var storeId = this.refs.storeId.value;
+    this.history.pushState(null, '/store/' + storeId);
+  },
   render: function(){
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" ref="storeId" />
+        <input type="text" ref="storeId" defaultValue={h.getFunName()}/>
         <input type="Submit" />
       </form>
     )

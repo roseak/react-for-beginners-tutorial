394f2436d4966597f1ee5b747083c166d3a194ea
roseak <rose.kohn@gmail.com>
2016-04-29 11:51:00 -0600

    Start change of classes and add in react-mixin.

--- scripts/components/StorePicker.js
+++ scripts/components/StorePicker.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { History } from 'react-router'
 import h from '../helpers';
+import reactMixin from 'react-mixin';
 
 
-var StorePicker = React.createClass({
-  mixins: [History],
-  goToStore: function(event) {
+class StorePicker extends React.Component {
+
+  goToStore(event) {
     event.preventDefault();
     var storeId = this.refs.storeId.value;
     this.history.pushState(null, '/store/' + storeId);
-  },
-  render: function(){
+  }
+
+  render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
@@ -19,6 +21,8 @@ var StorePicker = React.createClass({
       </form>
     )
   }
-});
+}
+
+reactMixin.onClass(StorePicker, History);
 
 export default StorePicker;
